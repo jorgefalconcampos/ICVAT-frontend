ede9e8ab202f044c2c89fcb7d601f28661b93174
Jorge Falcón <jorgef_campos@outlook.com>
2022-01-11 14:43:43 -0600

    modified footer

--- src/components/landing/FooterSection.vue
+++ src/components/landing/FooterSection.vue
@@ -1,43 +1,24 @@
 <template>
-<div>
-   <div class="svg-border-waves text-white">
-      <v-img src="~@/assets/img/borderWavesBlue.svg"/>
+    <div>
+        <div class="svg-border-waves text-white">
+            <v-img src="~@/assets/img/borderWavesBlue.svg"/>
+        </div>
+        <v-footer dark padless>
+            <v-card flat tile class="colorBlue white--text text-center">
+                <v-card-text class="white--text pt-5">Hecho con ❤️ en el Instituto Politécnico Nacional</v-card-text>
+                <v-divider></v-divider>
+                <v-card-text >{{ new Date().getFullYear() }} | <strong>ICVAT </strong>team</v-card-text>
+            </v-card>
+        </v-footer>  
     </div>
-
-
-  <v-footer dark padless>
-    <v-card flat tile class="secondary white--text text-center">
-      <v-card-text>
-        <v-btn
-          v-for="(icon, i) in icons"
-          :key="i"
-          class="mx-4 white--text"
-          :href="icon.link"
-          target="_blank"
-          icon
-        >
-          <v-icon size="24px">{{ icon.text }}</v-icon>
-        </v-btn>
-      </v-card-text>
-
-      <v-card-text class="white--text pt-0">
-        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet.
-        Mauris cursus commodo interdum.
-      </v-card-text>
-
-      <v-divider></v-divider>
-
-      <v-card-text class="white--text">
-        {{ new Date().getFullYear() }} — <strong>Calango WEB</strong>
-      </v-card-text>
-    </v-card>
-  </v-footer>  
-</div>
 </template>
 
 <style scoped>
 .v-card {
   width: 100%;
+}
+.colorBlue{
+    background-color: #171B34;
 }
 .svg-border-waves .v-image {
   background-color: #f4f7f5;
@@ -53,24 +34,6 @@
 <script>
 export default {
   data: () => ({
-    icons: [
-      {
-        text: "mdi-facebook",
-        link: "",
-      },
-      {
-        text: "mdi-twitter",
-        link: "",
-      },
-      {
-        text: "mdi-linkedin",
-        link: "",
-      },
-      {
-        text: "mdi-instagram",
-        link: "https://instagram.com/joabson_arley/",
-      },
-    ],
   }),
 };
 </script>
