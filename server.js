871161d3f122017680ec66d671a0e869edcd9732
Jorge Falcón <jorgef_campos@outlook.com>
2022-01-15 22:37:16 -0600

    changes in server file

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const serveStatic = require('serve-static');
 const path = require('path');
 const app = express();
 
-app.use(serveStatic(path.join(__dirname, 'dist')));
+// app.use(serveStatic(path.join(__dirname, 'dist')));
+
+app.use('/', serveStatic(path.join(__dirname, 'dist')));
+
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
 
 const port = process.env.PORT || 3000;
 
