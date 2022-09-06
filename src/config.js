682a044340ee254105186856bbd8672acd3e49ae
Jorge Falcón <jorgef_campos@outlook.com>
2022-09-05 22:34:22 -0500

    hcode

--- src/config.js
+++ src/config.js
@@ -1,11 +1,3 @@
-let baseUrl = "";
-
-baseUrl = process.env.CURRENT_ENV;
-
-// if (process.env.CURRENT_ENV === "prod") {
-//   baseUrl = "https://icvat-iqy65.ondigitalocean.app/api";
-// } else {
-//   baseUrl = "http://localhost:8000/api";
-// }
+let baseUrl = "https://icvat-iqy65.ondigitalocean.app/api";
 
 export const apiHost = baseUrl;
